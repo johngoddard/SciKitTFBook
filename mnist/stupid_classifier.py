47b734b364711d8c99fa43b9576367b9deb5b604
John Goddard <johngoddard243@gmail.com>
2017-08-21 21:53:23 -0700

    add mnist dir

---
+++ mnist/stupid_classifier.py
@@ -0,0 +1,8 @@
+from sklearn.base import BaseEstimator;
+import numpy as np
+
+class StupidClassifier(BaseEstimator):
+    def fit(self, X, y=None):
+        pass
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
